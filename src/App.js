bd5354c5ab0ac597005a0274ad7a2d2994494c45
Darshan Patil <128298312+DarshanPatil113@users.noreply.github.com>
2025-01-19 17:40:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,51 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css'
+
 class App extends Component {
-    state = {advice:'',
-        color:'black',
-        color1:'black',
-        colorR:0,
-        colorG:0,
-        colorB:0,
+    state = {
+        advice: '',
+        color: 'black',
+        colorR: 0,
+        colorG: 0,
+        colorB: 0,
+        prevAdvice: '' // Store the previous advice to compare
     };
-    componentDidMount(){
+
+    componentDidMount() {
         this.fetchAdvice(); 
     }
-    fetchAdvice =()=>{
+
+    fetchAdvice = () => {
         axios.get('https://api.adviceslip.com/advice')
-            .then((response)=>{
-                const {advice}=response.data.slip;
+            .then((response) => {
+                const { advice } = response.data.slip;
                 console.log(advice);
-                this.setState({advice:advice});
+
+                // Only update the color if the advice changes
+                if (advice !== this.state.prevAdvice) {
+                    this.setState({
+                        advice: advice,
+                        prevAdvice: advice, // Update prevAdvice to the new advice
+                        colorR: (this.state.colorR + 10) % 250,
+                        colorG: (this.state.colorG + 20) % 250,
+                        colorB: (this.state.colorB + 30) % 250,
+                        color: `rgb(${Math.floor(Math.random() * this.state.colorR)}, ${Math.floor(Math.random() * this.state.colorG)}, ${Math.floor(Math.random() * this.state.colorB)})`
+                    });
+                }
             })
-            .catch((error)=>{
+            .catch((error) => {
                 console.log(error);
             });
-            this.setState({
-                colorR:(this.state.colorR+10)%250,
-                colorG:(this.state.colorG+20)%250,
-                colorB:(this.state.colorB+30)%250,
-                color1: `rgb(${Math.floor(Math.random()*this.state.colorR)}, ${Math.floor(Math.random()*this.state.colorG)}, ${Math.floor(Math.random()*this.state.colorB)})`,
-                color: `rgb(${Math.floor(Math.random()*this.state.colorR)}, ${Math.floor(Math.random()*this.state.colorG)}, ${Math.floor(Math.random()*this.state.colorB)})`
-            });
-              
-        
-    }
-    clickFirst=()=>{
-        this.fetchAdvice();
-    }
+    };
+
     render() {
-        const {advice}=this.state;
+        const { advice, color } = this.state;
         return (
             <div className="app">
                 <div className='card'>
                     <h1 className='heading'>{advice}</h1>
-                    <button className='button1' style={{backgroundColor : this.state.color1}} onClick={this.clickFirst} >
-                        <span>
-                            CLICK ME FIRST!
-                        </span>
-                    </button>
-                    <button className='button' style={{backgroundColor : this.state.color}}>
+                    <button className='button' style={{ backgroundColor: color }} onClick={this.fetchAdvice}>
                         <span>
                             GIVE ME ADVICE!
                         </span>
